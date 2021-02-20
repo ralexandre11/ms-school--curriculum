d2303ef01692a3f390011dfd4df527be403bff67
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-20 11:35:35 -0300

    Created Project

---
+++ src/main/java/com/ribeiro/schoolcurriculum/SchoolCurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.ribeiro.schoolcurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolCurriculumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ribeiro/schoolcurriculum/SchoolCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ribeiro.schoolcurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
